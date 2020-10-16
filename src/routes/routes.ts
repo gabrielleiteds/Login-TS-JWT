5a5645dfde5220daf0596dbfe1a231e9a082b4b2
leitegs <leitedasilvag@gmail.com>
2020-10-16 12:41:00 -0300

    create routes for creation of users

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,9 +1,13 @@
 import { Response, Request, Router } from 'express';
+import bcrypt from 'bcryptjs';
+import UserController from '../controllers/UserController';
 
 const routes = Router()
 
 routes.get('/', (request: Request, response: Response) => {
   return response.json({ message: 'Hello World' })
 })
+
+routes.post('/user', UserController.create)
 
 export default routes; 
