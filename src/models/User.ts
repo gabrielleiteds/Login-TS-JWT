c0824dc958c0b2f9ce62cc9af7034487dad01a4a
leitegs <leitedasilvag@gmail.com>
2020-10-16 22:08:43 -0300

    implement hash with bcrypt in password

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,5 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, BeforeInsert, BeforeUpdate } from 'typeorm';
+import bcrypt from 'bcryptjs';
 
 @Entity('users')
 export default class User {
@@ -6,8 +7,17 @@ export default class User {
   id: number;
 
   @Column()
+  name: string;
+
+  @Column()
   email: string;
-  
+
   @Column()
   password: string;
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  hash_password() { 
+    this.password = bcrypt.hashSync(this.password, 8)
+  } //function for generate hash in password
 }
