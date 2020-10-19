6f75a374598a2f3f9db2f0acaf0dec3c0a289b6b
leitegs <leitedasilvag@gmail.com>
2020-10-18 23:10:36 -0300

    create routes for authentication

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,11 +1,15 @@
 import { Response, Request, Router } from 'express';
 import bcrypt from 'bcryptjs';
 import UserController from '../controllers/UserController';
+import AuthController from '../controllers/AuthController';
 
 const routes = Router()
 
 //CRUD for users
 routes.get('/', UserController.index)
 routes.post('/user', UserController.create)
+
+//authenticate
+routes.post('/auth', AuthController.authenticate)
 
 export default routes; 
